c8ea15bbdfe7b7c7312743d75756f2d312f81b23
Admin <2762713521@qq.com>
2024-11-30 17:12:01 +0800

    start binding

--- TreeTable_test.go
+++ TreeTable_test.go
@@ -5,4 +5,14 @@ import (
 )
 
 func TestTreeTable_ContextMenuItem(t1 *testing.T) {
+	//		"CopyRow",
+	//		"ConvertToContainer",
+	//		"ConvertToNonContainer",
+	//		"New",
+	//		"NewContainer",
+	//		"Delete",
+	//		"Duplicate",
+	//		"Edit",
+	//		"OpenAll",
+	//		"CloseAll",
 }
