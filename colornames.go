8ed495fb6b699d21e630bf36f83ed002bdc19348
Admin <2762713521@qq.com>
2025-02-26 15:33:22 +0800

    fix https://github.com/ddkwork/ux/issues/21

--- colornames.go
+++ colornames.go
@@ -784,5 +784,6 @@ var (
 	DividerFg       = color.NRGBA{R: 88, G: 88, B: 88, A: 255}
 	BackgroundColor = color.NRGBA{R: 54, G: 54, B: 54, A: 255}
 	ColorPink       = color.NRGBA{R: 166, G: 96, B: 192, A: 255}
-	ColorHeaderFg   = color.NRGBA{R: 76, G: 76, B: 76, A: 255}
+	ColorHeaderFg   = color.NRGBA{R: 70, G: 70, B: 70, A: 255}
+	//ColorHeaderFg = color.NRGBA{R: 76, G: 76, B: 76, A: 255}
 )
