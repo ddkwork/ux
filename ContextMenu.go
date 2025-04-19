59506224ff20815ef3e3f17e28d4a88884453e2e
Admin <2762713521@qq.com>
2025-04-19 15:26:13 +0800

    fix https://github.com/ddkwork/ux/issues/25

--- ContextMenu.go
+++ ContextMenu.go
@@ -72,6 +72,7 @@ func (m *ContextMenu) OnClicked(gtx C) {
 	}
 }
 
+// 测试用例，现在不需要了
 func (m *ContextMenu) drawRowDefault(gtx layout.Context, rowClick *widget.Clickable, index int) layout.Dimensions {
 	if event, b := gtx.Event(pointer.Filter{Target: rowClick, Kinds: pointer.Press | pointer.Release}); b {
 		if e, ok := event.(pointer.Event); ok {
@@ -90,7 +91,9 @@ func (m *ContextMenu) drawRowDefault(gtx layout.Context, rowClick *widget.Clicka
 	return buttonStyle.Layout(gtx)
 }
 
-func (m *ContextMenu) LayoutRow(gtx layout.Context, index int) layout.Dimensions { //官方表格控件已经有水平和垂直滚动条,所以不使用list布局，兼用一下
+// LayoutRow 对先行表格不感兴趣，不过在demo/other/hashicon/example/main.go下已经通过测试，不会去维护官方那个表格，不好用
+// 官方表格控件已经有水平和垂直滚动条,所以不使用list布局，兼用一下
+func (m *ContextMenu) LayoutRow(gtx layout.Context, index int) layout.Dimensions {
 	return layout.Stack{}.Layout(gtx,
 		layout.Stacked(func(gtx C) D {
 			rowClick := &m.RowClicks[index]
@@ -118,53 +121,14 @@ func (m *ContextMenu) LayoutRow(gtx layout.Context, index int) layout.Dimensions
 				gtx.Constraints.Min = image.Point{}
 				m.OnClicked(gtx)
 				//return m.drawContextArea(gtx, th)
-				return component.Menu(th, &m.MenuState).Layout(gtx) //所有行的item公用一个popup菜单而不是每行popup一个
+				return component.Menu(th, &m.MenuState).Layout(gtx) //所有行的item共用一个popup菜单而不是每行popup一个
 			})
 		}),
 	)
 }
 
-func (m *ContextMenu) LayoutOld(gtx layout.Context) layout.Dimensions {
-	return material.List(th, &m.List).Layout(gtx, len(m.RowClicks), func(gtx layout.Context, index int) layout.Dimensions {
-		return layout.Stack{}.Layout(gtx,
-			layout.Stacked(func(gtx C) D {
-				rowClick := &m.RowClicks[index]
-				return material.Clickable(gtx, rowClick, func(gtx layout.Context) layout.Dimensions {
-					gtx.Constraints.Min.X = gtx.Constraints.Max.X
-					if event, b := gtx.Event(pointer.Filter{Target: rowClick, Kinds: pointer.Press | pointer.Release}); b {
-						if e, ok := event.(pointer.Event); ok {
-							if e.Kind == pointer.Press {
-								switch {
-								case e.Buttons.Contain(pointer.ButtonPrimary):
-									m.ClickedRowindex = index
-									println("Row selected (left click) " + strconv.Itoa(index))
-								case e.Buttons.Contain(pointer.ButtonSecondary):
-									m.ClickedRowindex = index
-									println("Row selected (right click)" + strconv.Itoa(index))
-								}
-							}
-						}
-					}
-					if m.DrawRow == nil {
-						return m.drawRowDefault(gtx, rowClick, index)
-					}
-					return m.DrawRow(gtx, index)
-				})
-			}),
-			layout.Expanded(func(gtx C) D {
-				return m.ContextArea.Layout(gtx, func(gtx C) D {
-					gtx.Constraints.Min = image.Point{}
-					m.OnClicked(gtx)
-					return m.drawContextArea(gtx, th)
-					return component.Menu(th, &m.MenuState).Layout(gtx) //所有行的item公用一个popup菜单而不是每行popup一个
-				})
-			}),
-		)
-	})
-}
-
+// Layout 线性的list，表格以及非线性的树形表格(核心:直接rootRow当做线性表格即可，顶层调用menu布局。转不过弯来一直去处理容器节点是否渲染menu布局的问题)均通过测试
 func (m *ContextMenu) Layout(gtx layout.Context) layout.Dimensions {
-	//return m.LayoutOld(gtx)
 	return layout.Stack{}.Layout(gtx,
 		layout.Stacked(func(gtx C) D {
 			return material.List(th, &m.List).Layout(gtx, len(m.RowClicks), func(gtx layout.Context, index int) layout.Dimensions {
@@ -176,7 +140,7 @@ func (m *ContextMenu) Layout(gtx layout.Context) layout.Dimensions {
 							if e.Kind == pointer.Press {
 								switch {
 								case e.Buttons.Contain(pointer.ButtonPrimary):
-									m.ClickedRowindex = index
+									m.ClickedRowindex = index //todo 移除树形表格的这个意思的字段?
 									println("Row selected (left click) " + strconv.Itoa(index))
 								case e.Buttons.Contain(pointer.ButtonSecondary):
 									m.ClickedRowindex = index
@@ -197,7 +161,7 @@ func (m *ContextMenu) Layout(gtx layout.Context) layout.Dimensions {
 				gtx.Constraints.Min = image.Point{}
 				m.OnClicked(gtx)
 				return m.drawContextArea(gtx, th)
-				return component.Menu(th, &m.MenuState).Layout(gtx) //所有行的item公用一个popup菜单而不是每行popup一个
+				return component.Menu(th, &m.MenuState).Layout(gtx) //所有行的item共用一个popup菜单而不是每行popup一个
 			})
 		}),
 	)
