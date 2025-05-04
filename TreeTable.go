d163790e82d9ccc33aee40d213db8603c907c14f
Admin <2762713521@qq.com>
2025-05-04 15:42:16 +0800

    通过SyncToModel缓存rootRowsWidget的状态来解决上下文菜单每次需要分配rows内存的性能问题

--- TreeTable.go
+++ TreeTable.go
@@ -1955,13 +1955,13 @@ const (
 )
 
 func (t *TreeTable[T]) maxColumnCellTextWidth() unit.Dp {
-	HierarchyIndent := unit.Dp(1)
-	DividerWidth := align.StringWidth[unit.Dp](" │ ")
+	hierarchyIndent := unit.Dp(1)
+	dividerWidth := align.StringWidth[unit.Dp](" │ ")
 	iconWidth := align.StringWidth[unit.Dp](childPrefix)
-	return t.MaxDepth()*HierarchyIndent + // 最大深度的左缩进
+	return t.MaxDepth()*hierarchyIndent + // 最大深度的左缩进
 		iconWidth + // 图标宽度,不管深度是多少，每一行都只会有一个层级图标
 		t.maxColumnTextWidths[0] + 5 + // (8 * 2) + 20 + // 左右padding,20是sort图标的宽度或者容器节点求和的文本宽度
-		DividerWidth // 列分隔条宽度
+		dividerWidth // 列分隔条宽度
 }
 
 func (t *TreeTable[T]) Format() *stream.Buffer {
